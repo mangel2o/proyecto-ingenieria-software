7d8a8d84ac04db69231f6276ce4df43c3011b699
PedroMtz <33788872+PedroMtz20@users.noreply.github.com>
2021-03-19 20:09:08 -0600

    Actividad 5

--- seeker/Program.cs
+++ seeker/Program.cs
@@ -4,22 +4,20 @@ namespace seeker{
    class Program{
       static void Main(string[] args){
          string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-         path += "\\GitHub\\proyecto-ingenieria-software\\seeker";
+         path = "D:\\Documents\\GitHub\\proyecto-ingenieria-software\\seeker";
 
          /*
          ActOne act1 = new ActOne();
          act1.executeProgram(path);
          
-
          ActTwo act2 = new ActTwo();
          act2.executeProgram(path);
          
          ActThree act3 = new ActThree();
          act3.executeProgram(path);
          */
-         ActFour act4 = new ActFour();
-         act4.executeProgram(path);
-         
+         ActFive act5 = new ActFive();
+         act5.executeProgram(path);       
       }
    }
 }
