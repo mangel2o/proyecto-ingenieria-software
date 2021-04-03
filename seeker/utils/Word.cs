b1a2ca82c9c46ded7e75bc87489067929a0bc00a
PedroMtz <33788872+PedroMtz20@users.noreply.github.com>
2021-04-03 14:06:41 -0600

    Words.cs en el lugar correcto

--- seeker/utils/Word.cs
+++ seeker/utils/Word.cs
@@ -7,22 +7,26 @@ public class Word{
    public int freqFile;
    public string reference;
    public ArrayList references;
+   public Dictionary<string,int> KeyFileRepetitions;
 
    public Word(string content){
       this.content = content;
       this.references = new ArrayList();
+      this.KeyFileRepetitions = new Dictionary<string, int>();
    }
 
    public Word(string content, string reference){
       this.content = content;
       this.reference = reference;
       this.references = new ArrayList();
+      this.KeyFileRepetitions = new Dictionary<string, int>();
    }
 
    public Word(string content, int freq){
       this.content = content;
       this.freq = freq;
       this.references = new ArrayList();
+      this.KeyFileRepetitions = new Dictionary<string, int>();
    }
 
    public Word(string content, int freq, int freqFile){
@@ -30,5 +34,6 @@ public class Word{
       this.freq = freq;
       this.freqFile = freqFile;
       this.references = new ArrayList();
+      this.KeyFileRepetitions = new Dictionary<string, int>();
    }
 }
